0758f6ef805426672da922fa0d60f4900a439f16
citadin88 <31966923+citadin88@users.noreply.github.com>
2023-12-15 23:35:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-function startInterval() {
-    setInterval(function () {
-        var _0x14e2f0 = _0x21d4,
-            _0x383d26 = clickTargets[Math[_0x14e2f0(0x198)](Math[_0x14e2f0(0x1a0)]() * clickTargets[_0x14e2f0(0x194)])],
-            _0x355f8a = _0x383d26['getBoundingClientRect'](),
-            _0x53e9be = _0x355f8a[_0x14e2f0(0x19b)] + Math[_0x14e2f0(0x1a0)]() * _0x355f8a['width'],
-            _0x984dc7 = _0x355f8a['top'] + Math[_0x14e2f0(0x1a0)]() * _0x355f8a[_0x14e2f0(0x1a3)];
-        simulateMouseClick(_0x53e9be, _0x984dc7);
-    }, 0x40);
-}
+function simulateDelayedClick(x, y) {
+      var clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: y
+      });
+      var element = document.elementFromPoint(x, y);
+      if (element) {
+        element.dispatchEvent(clickEvent);
+      }
+    }
 
-// Execute startInterval when the page loads
-window.onload = function () {
-    startInterval();
-};
+    function simulateMouseClick(x, y) {
+      var mouseDownEvent = new MouseEvent('mousedown', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: y
+      });
+
+      var mouseUpEvent = new MouseEvent('mouseup', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: y
+      });
+
+      var body = document.body;
+      body.dispatchEvent(mouseDownEvent);
+
+      setTimeout(function() {
+        body.dispatchEvent(mouseUpEvent);
+        setTimeout(function() {
+          simulateDelayedClick(x, y);
+        }, 50); // Adjust the delay for better results
+      }, 50); // Adjust the delay for better results
+    }
+
+    // Function to trigger the interval
+    function startInterval() {
+      setInterval(function() {
+        var randomX = Math.random() * window.innerWidth;
+        var randomY = Math.random() * window.innerHeight;
+        simulateMouseClick(randomX, randomY);
+      }, 2000); // 2000 milliseconds = 2 seconds
+    }
+
+    // Delayed start of the interval
+    setTimeout(startInterval, 2000); // Start after 2 seconds
