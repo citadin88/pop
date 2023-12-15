a95cdc11888f19f1aa426e4bec60d1662f5961d5
citadin88 <31966923+citadin88@users.noreply.github.com>
2023-12-16 00:47:22 +0100

    Add files via upload

---
+++ sropt.js
@@ -0,0 +1,55 @@
+// script.js
+
+function simulateDelayedClick(x, y) {
+  var clickEvent = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+    clientX: x,
+    clientY: y
+  });
+  var element = document.elementFromPoint(x, y);
+  if (element) {
+    element.dispatchEvent(clickEvent);
+  }
+}
+
+function simulateMouseClick(x, y) {
+  var mouseDownEvent = new MouseEvent('mousedown', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+    clientX: x,
+    clientY: y
+  });
+
+  var mouseUpEvent = new MouseEvent('mouseup', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+    clientX: x,
+    clientY: y
+  });
+
+  var body = document.body;
+  body.dispatchEvent(mouseDownEvent);
+
+  setTimeout(function() {
+    body.dispatchEvent(mouseUpEvent);
+    setTimeout(function() {
+      simulateDelayedClick(x, y);
+    }, 50); // Adjust the delay for better results
+  }, 50); // Adjust the delay for better results
+}
+
+// Function to trigger the interval
+function startInterval() {
+  setInterval(function() {
+    var randomX = Math.random() * window.innerWidth;
+    var randomY = Math.random() * window.innerHeight;
+    simulateMouseClick(randomX, randomY);
+  }, 2000); // 2000 milliseconds = 2 seconds
+}
+
+// Delayed start of the interval
+setTimeout(startInterval, 2000); // Start after 2 seconds
